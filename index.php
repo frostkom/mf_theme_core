66270f160e4d2b493b9ae82e2552c6e7e74e0e09
frostkom <martin.fors@frostkom.se>
2016-06-22 11:40:56 +0200

    Update z-index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Theme Core
 Plugin URI: https://github.com/frostkom/mf_theme_core
 Description: 
-Version: 4.6.2
+Version: 4.6.3
 Author: Martin Fors
 Author URI: http://frostkom.se
 Text Domain: lang_theme_core
