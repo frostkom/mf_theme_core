51fba591888f0690d4e43010b24250abb772c9ae
frostkom <martin.fors@frostkom.se>
2017-09-11 14:37:59 +0200

    Clean up widget

--- include/classes.php
+++ include/classes.php
@@ -11,14 +11,19 @@ class widget_theme_core_search extends WP_Widget
 
 		$control_ops = array('id_base' => 'theme-search-widget');
 
+		$this->arr_default = array(
+			'search_placeholder' => "",
+			'search_animate' => 'yes',
+		);
+
 		parent::__construct('theme-search-widget', __("Search", 'lang_theme_core'), $widget_ops, $control_ops);
 	}
 
 	function widget($args, $instance)
 	{
-		global $wpdb;
-
 		extract($args);
+
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		echo get_search_theme_core(array('placeholder' => $instance['search_placeholder'], 'animate' => (isset($instance['search_animate']) ? $instance['search_animate'] : 'yes')));
 	}
@@ -27,19 +32,17 @@ class widget_theme_core_search extends WP_Widget
 	{
 		$instance = $old_instance;
 
+		$new_instance = wp_parse_args((array)$new_instance, $this->arr_default);
+
 		$instance['search_placeholder'] = strip_tags($new_instance['search_placeholder']);
-		$instance['search_animate'] = isset($new_instance['search_animate']) ? strip_tags($new_instance['search_animate']) : 'yes';
+		$instance['search_animate'] = strip_tags($new_instance['search_animate']);
 
 		return $instance;
 	}
 
 	function form($instance)
 	{
-		$defaults = array(
-			'search_placeholder' => "",
-			'search_animate' => 'yes',
-		);
-		$instance = wp_parse_args((array)$instance, $defaults);
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		echo "<div class='mf_form'>"
 			.show_textfield(array('name' => $this->get_field_name('search_placeholder'), 'text' => __("Placeholder", 'lang_theme_core'), 'value' => $instance['search_placeholder']))
@@ -59,6 +62,11 @@ class widget_theme_core_news extends WP_Widget
 
 		$control_ops = array('id_base' => 'theme-news-widget');
 
+		$this->arr_default = array(
+			'news_title' => "",
+			'news_amount' => 3,
+		);
+
 		parent::__construct('theme-news-widget', __("News", 'lang_theme_core'), $widget_ops, $control_ops);
 	}
 
@@ -67,6 +75,8 @@ class widget_theme_core_news extends WP_Widget
 		global $wpdb;
 
 		extract($args);
+
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		$arr_news = array();
 
@@ -138,6 +148,8 @@ class widget_theme_core_news extends WP_Widget
 	{
 		$instance = $old_instance;
 
+		$new_instance = wp_parse_args((array)$new_instance, $this->arr_default);
+
 		$instance['news_title'] = strip_tags($new_instance['news_title']);
 		$instance['news_amount'] = strip_tags($new_instance['news_amount']);
 
@@ -146,11 +158,7 @@ class widget_theme_core_news extends WP_Widget
 
 	function form($instance)
 	{
-		$defaults = array(
-			'news_title' => "",
-			'news_amount' => 3,
-		);
-		$instance = wp_parse_args((array)$instance, $defaults);
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		echo "<div class='mf_form'>"
 			.show_textfield(array('name' => $this->get_field_name('news_title'), 'text' => __("Title", 'lang_theme_core'), 'value' => $instance['news_title']))
@@ -168,9 +176,12 @@ class widget_theme_core_promo extends WP_Widget
 			'description' => __("Promote Pages", 'lang_theme_core')
 		);
 
-		$control_ops = array('id_base' => 'theme-promo-widget');
+		$this->arr_default = array(
+			'promo_title' => "",
+			'promo_include' => array(),
+		);
 
-		parent::__construct('theme-promo-widget', __("Promotion", 'lang_theme_core'), $widget_ops, $control_ops);
+		parent::__construct('theme-promo-widget', __("Promotion", 'lang_theme_core'), $widget_ops);
 	}
 
 	function widget($args, $instance)
@@ -179,7 +190,9 @@ class widget_theme_core_promo extends WP_Widget
 
 		extract($args);
 
-		if(isset($instance['promo_include']) && count($instance['promo_include']) > 0)
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
+
+		if(count($instance['promo_include']) > 0)
 		{
 			$arr_pages = array();
 
@@ -265,19 +278,17 @@ class widget_theme_core_promo extends WP_Widget
 	{
 		$instance = $old_instance;
 
+		$new_instance = wp_parse_args((array)$new_instance, $this->arr_default);
+
 		$instance['promo_title'] = strip_tags($new_instance['promo_title']);
-		$instance['promo_include'] = isset($new_instance['promo_include']) ? $new_instance['promo_include'] : array();
+		$instance['promo_include'] = $new_instance['promo_include'];
 
 		return $instance;
 	}
 
 	function form($instance)
 	{
-		$defaults = array(
-			'promo_title' => "",
-			'promo_include' => array(),
-		);
-		$instance = wp_parse_args((array)$instance, $defaults);
+		$instance = wp_parse_args((array)$instance, $this->arr_default);
 
 		$arr_data = array();
 		get_post_children(array('post_type' => 'page', 'order_by' => 'post_title'), $arr_data);
