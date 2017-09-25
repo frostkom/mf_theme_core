76c9699265b713636d0ee9202ff96d33e640fb86
frostkom <martin.fors@frostkom.se>
2017-09-25 11:25:19 +0200

    Remove force relative URLs

--- include/functions.php
+++ include/functions.php
@@ -126,10 +126,10 @@ function header_theme_core()
 {
 	require_user_login();
 
-	if(!is_feed() && !get_query_var('sitemap') && get_option('setting_strip_domain') == 1)
+	/*if(!is_feed() && !get_query_var('sitemap') && get_option('setting_strip_domain') == 1)
 	{
 		ob_start("strip_domain_from_content");
-	}
+	}*/
 }
 
 function get_wp_title()
@@ -634,7 +634,7 @@ function settings_theme_core()
 
 		else
 		{*/
-			$arr_settings['setting_strip_domain'] = __("Force relative URLs", 'lang_theme_core');
+			//$arr_settings['setting_strip_domain'] = __("Force relative URLs", 'lang_theme_core');
 		//}
 
 		if(is_plugin_active("mf_analytics/index.php") && (get_option('setting_analytics_google') != '' || get_option('setting_analytics_clicky') != ''))
@@ -702,13 +702,13 @@ function setting_scroll_to_top_callback()
 	echo show_select(array('data' => get_yes_no_for_select(), 'name' => $setting_key, 'value' => $option));
 }
 
-function setting_strip_domain_callback()
+/*function setting_strip_domain_callback()
 {
 	$setting_key = get_setting_key(__FUNCTION__);
 	$option = get_option_or_default($setting_key, 0);
 
 	echo show_select(array('data' => get_yes_no_for_select(array('return_integer' => true)), 'name' => $setting_key, 'value' => $option));
-}
+}*/
 
 function setting_cookie_info_callback()
 {
@@ -1567,13 +1567,13 @@ function admin_bar_theme_core()
 	}
 }
 
-function init_style_theme_core()
+/*function init_style_theme_core()
 {
 	if(get_option('setting_strip_domain') == 1)
 	{
 		ob_start("strip_domain_from_content");
 	}
-}
+}*/
 
 function mf_unregister_widget($id)
 {
@@ -1651,10 +1651,10 @@ function get_search_theme_core($data = array())
 	."</form>";
 }
 
-function strip_domain_from_content($html)
+/*function strip_domain_from_content($html)
 {
 	$site_url = get_option('siteurl');
 	$site_url_alt = (substr($site_url, 0, 5) == "https" ? str_replace("https:", "http:", $site_url) : str_replace("http:", "https:", $site_url));
 
 	return str_replace(array($site_url, $site_url_alt), "", $html);
-}
+}*/
