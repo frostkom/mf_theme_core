67710c7f71fe0fcdca2e4ba613b778fda4f6ebf6
frostkom <martin.fors@frostkom.se>
2017-12-06 09:51:56 +0100

    Do not use public style in admin anymore

--- include/functions.php
+++ include/functions.php
@@ -502,13 +502,12 @@ function gather_params($options_params)
 
 function init_theme_core()
 {
-	$plugin_include_url = plugin_dir_url(__FILE__);
-	$plugin_version = get_plugin_version(__FILE__);
-
-	mf_enqueue_style('style_theme_core', $plugin_include_url."style.css", $plugin_version);
-
 	if(!is_admin())
 	{
+		$plugin_include_url = plugin_dir_url(__FILE__);
+		$plugin_version = get_plugin_version(__FILE__);
+
+		mf_enqueue_style('style_theme_core', $plugin_include_url."style.css", $plugin_version);
 		mf_enqueue_script('script_theme_core', $plugin_include_url."script.js", $plugin_version);
 
 		if(get_option('setting_scroll_to_top') == 'yes')
