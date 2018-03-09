2afdbf9515a440fb595423840d33fb2dc39fc998
frostkom <martin.fors@frostkom.se>
2018-03-09 14:23:41 +0100

    Display href after links when printing

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Theme Core
 Plugin URI: https://github.com/frostkom/mf_theme_core
 Description: 
-Version: 6.15.32
+Version: 6.15.33
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: http://frostkom.se
