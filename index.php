9e162b648b74301e6548d73104246bc4516b7b94
Martin Fors <martin.fors@frostkom.se>
2018-08-04 07:33:23 +0200

    Fix translation

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Theme Core
 Plugin URI: https://github.com/frostkom/mf_theme_core
 Description: 
-Version: 7.2.23
+Version: 7.2.24
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
