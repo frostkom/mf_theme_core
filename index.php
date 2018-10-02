b38fb007750b1d83834f887408f143053de8bc11
Martin Fors <martin.fors@frostkom.se>
2018-10-02 05:53:22 +0200

    Change button width in widget

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Theme Core
 Plugin URI: https://github.com/frostkom/mf_theme_core
 Description: 
-Version: 7.3.35
+Version: 7.3.36
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
