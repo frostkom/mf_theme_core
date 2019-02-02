3e6bb919f79e9b4034c891b5f0779b951291e788
frostkom <martin.fors@frostkom.se>
2019-02-02 15:30:52 +0100

    Update API action

--- include/api/index.php
+++ include/api/index.php
@@ -20,42 +20,40 @@ $json_output = array();
 
 $type = check_var('type', 'char');
 
-$arr_input = explode("/", $type);
-
-$type_action = $arr_input[0];
-
-if($type_action == "get_style_source")
+switch($type)
 {
-	$theme = wp_get_theme();
+	case 'get_style_source':
+		$theme = wp_get_theme();
 
-	if($theme->exists())
-	{
-		$theme_dir_name = get_theme_dir_name();
-
-		list($upload_path, $upload_url) = get_uploads_folder($theme_dir_name);
-
-		$arr_backups = get_previous_backups_list($upload_path);
-		$count_temp = count($arr_backups);
-
-		if($count_temp > 0)
+		if($theme->exists())
 		{
-			$style_url = $upload_url.$arr_backups[0]['name'];
-			$style_changed = date("Y-m-d H:i:s", strtotime(substr($style_url, -17, 12)));
-		}
+			$theme_dir_name = get_theme_dir_name();
 
-		else
-		{
-			$style_url = "";
-			$style_changed = DEFAULT_DATE;
-		}
+			list($upload_path, $upload_url) = get_uploads_folder($theme_dir_name);
 
-		$json_output['success'] = true;
-		$json_output['response'] = array(
-			'theme_version' => $theme->get('Version'), //Deprecated
-			'style_changed' => $style_changed,
-			'style_url' => $style_url,
-		);
-	}
+			$arr_backups = get_previous_backups_list($upload_path);
+			$count_temp = count($arr_backups);
+
+			if($count_temp > 0)
+			{
+				$style_url = $upload_url.$arr_backups[0]['name'];
+				$style_changed = date("Y-m-d H:i:s", strtotime(substr($style_url, -17, 12)));
+			}
+
+			else
+			{
+				$style_url = "";
+				$style_changed = DEFAULT_DATE;
+			}
+
+			$json_output['success'] = true;
+			$json_output['response'] = array(
+				'theme_version' => $theme->get('Version'), //Deprecated
+				'style_changed' => $style_changed,
+				'style_url' => $style_url,
+			);
+		}
+	break;
 }
 
 echo json_encode($json_output);
