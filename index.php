9cdf656e0da7d0e582db2800feee6d35bea86575
frostkom <martin.fors@frostkom.se>
2020-05-04 11:25:48 +0200

    Remove old code

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: MF Theme Core
 Plugin URI: https://github.com/frostkom/mf_theme_core
 Description: 
-Version: 7.8.34
+Version: 7.8.35
 Licence: GPLv2 or later
 Author: Martin Fors
 Author URI: https://frostkom.se
