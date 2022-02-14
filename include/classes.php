1c3d86f3c14e3f53c5ad8dbd479cc39e1a4c0798
frostkom <martin.fors@frostkom.se>
2022-02-14 11:51:25 +0100

    Add option for full width on header

--- include/classes.php
+++ include/classes.php
@@ -1784,6 +1784,7 @@ class mf_theme_core
 		}
 
 		$options_params[] = array('category' => __("Header", 'lang_theme_core'), 'id' => 'mf_theme_header');
+			$options_params[] = array('type' => 'checkbox', 'id' => 'header_full_width', 'title' => __("Full Width", 'lang_theme_core'), 'default' => 1);
 			$options_params[] = array('type' => 'position', 'id' => 'header_fixed', 'title' => __("Position", 'lang_theme_core'), 'default' => 'relative');
 			$options_params[] = array('type' => 'text', 'id' => 'header_bg', 'title' => __("Background", 'lang_theme_core'));
 				$options_params[] = array('type' => 'color', 'id' => 'header_bg_color', 'title' => " - ".__("Color", 'lang_theme_core')); //, 'ignore_default_if' => 'body_bg', 'default' => '#eeeeee'
