634f571aee37534cf61099b44565ddb733a017e0
frostkom <martin.fors@frostkom.se>
2023-08-24 07:20:32 +0200

    Change image transition on hover

--- include/style.php
+++ include/style.php
@@ -388,7 +388,7 @@ echo "@media all
 							display: block;
 							height: 100%;
 							object-fit: cover;
-							transition: all .75s ease;
+							transition: all 1s ease;
 							width: 100%;
 						}
 
